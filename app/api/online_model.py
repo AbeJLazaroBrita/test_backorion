08a2c1be5d64107c2d5b370d66aa5170c55cf495
abraham_lazaro <abraham.lazaro@rappi.com>
2023-09-18 09:18:41 -0600

    first commit

---
+++ app/api/online_model.py
@@ -0,0 +1,11 @@
+from core.load.specificLoader import Loader
+from core.load.loadModel import PandasAIModel
+
+#Load datas
+Datas = Loader()
+Datas.loadData('regimen.csv', 'documentos.csv')
+
+#Save into json
+Datas.documentos.to_json('./documents.json', orient = 'records')
+
+model = PandasAIModel('openai', Datas)
